258985dfe0361b7053e600f9d907a8f5a62f82ef
ryotapb715@gmail.com <ryotapb715@gmail.com>
2021-12-28 12:25:01 +0900

    修正

--- Practice.java
+++ Practice.java
@@ -1,13 +1,13 @@
 public class Practice {
   public static void main (String[] args) {
-    for (int i = 0; i < 5; i++) {
-      for (int j = 0; j < 5 - (i+1); j++) {
-        System.out.println(" ");
+    for(int i=0; i<5; i++) {
+      for(int j=0; j<5-(i+1); j++) {
+        System.out.print(" ");
       }
-      for (int k = 0; k < (i+1) * 2 - 1; k++) {
-        System.out.println("*");
+      for(int k=0; k<(i+1)*2-1; k++) {
+        System.out.print("*");
       }
-      System.out.println("\n");
+      System.out.print("\n");
     }
   }
 }
